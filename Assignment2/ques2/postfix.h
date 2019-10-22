0160639474a1d27cc02da34b1443caccea1337b0
PoojaDP-23 <55285644+PoojaDP-23@users.noreply.github.com>
2019-10-22 20:05:20 +0530

    Create postfix.h

---
+++ Assignment2/ques2/postfix.h
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+
+struct Stack 
+{ 
+	int top; 
+	unsigned capacity; 
+	int* array; 
+}; 
+
+struct Stack* createStack( unsigned  ); 
+int isEmpty(struct Stack* );
+char peek(struct Stack*); 
+char pop(struct Stack* );
+void push(struct Stack* , char ); 
+int evaluatePostfix(char* );
+ 
